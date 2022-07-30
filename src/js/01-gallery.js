e537a8ee04e093e2283afdb642441765920f20f5
GrooT921 <rarapin96@gmail.com>
2022-07-30 11:48:43 +0200

    hw-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 // Change code below this line
+const galleryImg = document.querySelector('.gallery');
 
-console.log(galleryItems);
+
+const items = galleryItems.map(({preview , original, description})=>{
+    return `
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"/>
+        </a>
+    `;
+}).join('');
+galleryImg.insertAdjacentHTML('beforeend', items);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+// console.log(galleryItems);
